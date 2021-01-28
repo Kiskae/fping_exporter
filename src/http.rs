4450c51b6bbbae43177bb6094d9c2082dc038bbc
Kiskae <Kiskae@users.noreply.github.com>
2021-01-28 22:54:36 +0100

    add prometheus

--- src/http.rs
+++ src/http.rs
@@ -1,14 +1,27 @@
 use std::convert::Infallible;
 
+use prometheus::{Encoder, Registry, TextEncoder};
+use warp::{reply::with_header, Reply};
+
 use crate::args::MetricArgs;
+
+fn encode_metrics<E: Encoder + Default>(reg: &Registry) -> prometheus::Result<impl Reply> {
+    let enc: E = Default::default();
+    let mut out = Vec::new();
+    enc.encode(&reg.gather(), &mut out)?;
+    Ok(with_header(out, "Content-Type", enc.format_type()))
+}
 
 pub async fn publish_metrics(args: &MetricArgs) -> Result<(), warp::Error> {
     use warp::Filter;
 
-    let handler = || async {
-        //TODO: request summary update
-        //TODO: emit registry output
-        Ok::<_, Infallible>("well done!")
+    let handler = move || {
+        let reg = prometheus::default_registry();
+        async move {
+            //TODO: provide guarded access to Registry instead...
+            //TODO: request summary update from fping...
+            Ok::<_, Infallible>(encode_metrics::<TextEncoder>(&reg).unwrap())
+        }
     };
 
     let metrics = warp::path(args.path.clone())
